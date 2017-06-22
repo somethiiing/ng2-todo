8cc6c3a725174c5b463dc7671d4744b69c64a5c3
somethiiing <hello@wilsonyu.io>
2017-06-22 14:10:41 -0400

    add nav bar for todo list

---
+++ src/app/ui/app-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  styleUrls: ['app-bar.component.css'],
+  templateUrl: './app-bar.component.html'
+})
+
+export class AppBarComponent {};
