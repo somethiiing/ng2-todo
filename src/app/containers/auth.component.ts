5331e26c1a4034db5f7b4d4ff7d8bb5f1e18cf39
somethiiing <hello@wilsonyu.io>
2017-06-28 13:53:17 -0400

    hook up auth page

--- src/app/containers/auth.component.ts
+++ src/app/containers/auth.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'auth-container',
-  template: `     <div class="auth-container">
-      <h1>Auth</h1>
-    </div>
-  `
+  styleUrls: ['./auth.component.css'],
+  templateUrl: './auth.component.html'
 })
 
-export class Auth {}
+export class Auth {
+  user = {
+    email: '',
+    password: ''
+  }
+
+  mode: string = 'signin';
+  linkText: string = 'Don\'t have an account?';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  changeMode() {
+    if (this.mode === 'signin') {
+      this.mode = 'signup';
+      this.linkText = 'Already have an account?';
+    } else {
+      this.mode = 'signin';
+      this.linkText = 'Don\'t have an account?';
+    }
+  }
+
+  authenticate() {
+    this.authService.authenticate(this.mode, this.user)
+      .subscribe( () => this.router.navigate['']);
+  }
+
+
+}
